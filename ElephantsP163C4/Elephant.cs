64198c2ecd0e2e3066c7f109384aa448e6a7c5ef
ThoMaster300 <wi15b031@technikum-wien.at>
2016-10-12 11:43:51 +0200

    Add project files.

---
+++ ElephantsP163C4/Elephant.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ElephantsP163C4
+{
+    class Elephant
+    {
+        public string Name;
+        public int EarSize;
+
+
+        public void WhoAmI()
+        {
+            MessageBox.Show("My ears are " + EarSize + " tall!", Name + " says...");
+        }
+        public Elephant Swap(Elephant t)
+        {
+            Elephant temp = t;
+            t = this;
+            return t;
+        }
+
+    }
+}
